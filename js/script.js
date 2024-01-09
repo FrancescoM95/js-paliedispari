f03e8d50a5c2fba4c7555906f6f83a25ae78e75e
FrancescoM95 <mon95fran@gmail.com>
2024-01-09 16:28:18 +0100

    ex 1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log('JS OK');
+
+//* ERSERCIZIO 1 - PALINDROMA
+
+//^ FUNZIONE VERIFICA PALINDROMO 
+function isPalindromo(word) {
+    // Rimuovo spazi e caratteri maiuscoli
+    word = word.toLowerCase().trim();
+    // Inverto i caratteri della parola
+    const wordInverted = word.split('').reverse().join('');
+    // Verifico se è un palindrmomo
+    if (word === wordInverted) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Recupero il form dalla pagina
+const form = document.getElementById('formPalindromo');
+
+// Al submit del form richiamo la funzione verifica palindromo e stampo il risultato in pagina
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const parola = document.getElementById('parola').value;
+    const risultato = document.getElementById('risultato');
+
+    if (isPalindromo(parola)) {
+        risultato.textContent = 'La parola inserita è un palindromo.';
+    } else {
+        risultato.textContent = 'La parola inserita non è un palindromo.';
+    }
+});
